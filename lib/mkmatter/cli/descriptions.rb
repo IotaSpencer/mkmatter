c0c4c843d7402bd40cb5edddb862e0775295f9c9
Ken Spencer <ken@electrocode.net>
2018-02-14 21:02:12 -0500

    move descriptions to a module and color them

---
+++ lib/mkmatter/cli/descriptions.rb
@@ -0,0 +1,37 @@
+require 'highline'
+HighLine.colorize_strings
+
+module Mkmatter
+  module App
+    module Descriptions
+      PAGE = <<-PAGEDOC
+        `mkmatter page` will run you through making a jekyll page.
+
+        Given the above options/flags you can modify how the script
+
+        outputs your front matter, or whether to mark it as published.
+      
+      PAGEDOC
+      
+      POST = <<~POSTDOC
+        `mkmatter post` will run you through making a jekyll post.
+
+        Given the above options/flags you can modify how the script
+
+        outputs your front matter, and whether to mark it as published,
+
+        or as a draft and move it into `_drafts`.
+
+        NOTES:
+        
+
+          By default Jekyll will publish a post if `published` is omitted in
+
+          the front matter. So if you omit `--publish` you will publish,
+
+          so you have to explicitly use --no-publish to set `#{HighLine.color('published', :yellow)}: #{HighLine.color('false', :yellow, :bold)}`
+      
+      POSTDOC
+    end
+  end
+end
