635ce9e7ef15afd05e2a64fca1bd4085831628ee
Ken Spencer <ken@electrocode.net>
2018-03-03 07:42:41 -0500

    add which for checking if executables exists

--- lib/mkmatter/cli/methods.rb
+++ lib/mkmatter/cli/methods.rb
@@ -29,13 +29,25 @@ module Mkmatter
         end
       end
     end
-    
+
+    def Methods.which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable?(exe) && !File.directory?(exe)
+        }
+      end
+      return nil
+    end
+    # @param [String] editor An editor to use
+    # @param [String] file_path Path to file to open
     # @return [NilClass] nil
     def Methods.launch_editor(editor, file_path)
       hl = HighLine.new($stdin, $stderr, 80)
       if file_path
         if hl.agree("Would you like to open an editor? ('editor' command) ", true)
-          pid = spawn("#{} #{file_path}")
+          pid = spawn("#{editor} #{file_path}")
           Process.wait pid
         end
       end
