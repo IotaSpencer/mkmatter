6924cb02458ab78b86f33b3382c4cacaa1e0be98
Ken Spencer <me@iotaspencer.me>
2018-05-08 20:52:22 -0400

    change up order in tests

--- test/questions_test.rb
+++ test/questions_test.rb
@@ -13,22 +13,20 @@ class QuestionsTest < MiniTest::Test
     @terminal = HighLine.new(@input, @output)
     @questions = Mkmatter::Questions
   end
-  
+
   def teardown
     # Teardown something
   end
-  
+
   def test_that_post_questions_no_file_and_no_draft_works
-    @input << "some title\ny\nsome,tags,here,multi word too\nsome categories here\n1\n2\n"
+    @input << "some title\ny\nmd\nsome,tags,here,multi word too\nsome categories here\n1\n"
     @input.rewind
     @questions::Post.new(@terminal).ask
-    @input = StringIO.new
-    
+
   end
   def test_that_page_questions_no_file_and_no_draft_works
-    @input << "some title\ny\nsome,tags,here,multi word too\nsome categories here\n1\n2\n"
+    @input << "some title\ny\nmd\nsome,tags,here,multi word too\nsome categories here\n1\n"
     @input.rewind
     @questions::Page.new(@terminal).ask
-    @input = StringIO.new
   end
 end
