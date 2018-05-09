13653e81fd7d7191c0d419348a2c8d4d08c53b7b
Ken Spencer <me@iotaspencer.me>
2018-05-08 20:51:01 -0400

    add thor shell customizations

--- lib/mkmatter.rb
+++ lib/mkmatter.rb
@@ -6,7 +6,25 @@ require 'mkmatter/version'
 
 require 'json'
 require 'yaml'
+require 'active_support/all'
 # Main Module Declaration
 module Mkmatter
 
+end
+class Thor
+  module Shell
+    class Basic
+      def print_wrapped(message, options = {})
+        message.lstrip!
+        message.gsub!(/\n\s+/, "\n")
+        message = message.split("\n")
+        message.each do |line|
+          line.gsub!(/^------/, '      ') if line[0..5] == '------'
+          line.gsub!(/^----/, '    ') if line[0..3] == '----'
+          line.gsub!(/^--/, '  ') if line[0..1] == '--'
+          stdout.puts line
+        end
+      end
+    end
+  end
 end
